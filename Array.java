a5d19a4264482c248d82e254856d2688f56371d2
khanhanh <44283159+khanhanh@users.noreply.github.com>
2019-03-22 08:40:16 +0700

    Add files via upload

--- Array.java
+++ Array.java
@@ -1,24 +1,12 @@
-package Assignment3;
+package Assignment6;
 
-import Session2.NhanVien;
-
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Array {
-    public static void main(String[] args){
-        ArrayList<String> stringArray=new ArrayList<>();
-        ArrayList<PhanSo> frArray=new ArrayList<>();
-        frArray.add(new PhanSo(1,2));
-        frArray.add(new PhanSo(2,3));
-        frArray.add(new PhanSo(3,4));
+    public Integer array[]=new Integer[3];
 
-        for (int i=0;i<=frArray.size();i++){
-            frArray.get(i).inRaPhanSo();
-        }
-        for (PhanSo fr:frArray){
-            fr.inRaPhanSo();
-        }
-
+    public Array(Integer[] array) {
+        this.array = array;
     }
 }
+
